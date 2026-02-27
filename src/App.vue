c32b74860f3eb7152f06d5db0ef129ed4c951fb0
Md Arman <armanar6712@gmail.com>
2026-02-27 23:37:19 +0530

    UPDATE: Project Setup and Basic Layout

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import LandingLayout from '@/layouts/LandingLayout.vue'
+</script>
+
+<template>
+  <LandingLayout />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="h-screen flex items-center justify-center">
+    <div class="text-center flex-1">
+      <h1 class="text-4xl font-bold">Notely</h1>
+      <p class="text-lg">Take note, share and collaborate</p>
+      <button class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg">Get Started</button>
+    </div>
+  </div>
+</template>
